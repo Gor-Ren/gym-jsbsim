fca4dcc6e0b0c90c04bac1ae405cfa2adc20182f
Gordon <gor.rennie@gmail.com>
2018-06-19 16:00:28 +0100

    Implement loading of aircraft models

--- test/test_JsbSimWrapper.py
+++ test/test_JsbSimWrapper.py
@@ -7,8 +7,22 @@ class TestJsbSimWrapper(unittest.TestCase):
     def test_init_jsbsim(self):
         wrapped_sim = JsbSimWrapper()
         self.assertIsInstance(wrapped_sim.sim, jsbsim.FGFDMExec,
-                              msg=f"Expected JsbSimWrapper.sim to hold an "
-                              f"instance of JSBSim. Actual: {type(sim.sim)}")
+                              msg=f'Expected JsbSimWrapper.sim to hold an '
+                              'instance of JSBSim.')
+
+    def test_load_aircraft(self):
+        wrapped_sim = JsbSimWrapper()
+        # we expect simulation to init with no aircraft loaded
+        tmp = wrapped_sim.get_model_name()
+        self.assertIsNone(wrapped_sim.get_model_name())
+
+        # load an "X15" plane
+        model_name = 'X15'
+        wrapped_sim.load_model(model_name)
+        actual_name = wrapped_sim.get_model_name()
+
+        self.assertEqual(model_name, actual_name,
+                         msg=f'Unexpected aircraft model name after loading.')
 
 
 if __name__ == '__main__':
--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,36 @@
 import jsbsim
+import os
 
 class JsbSimWrapper(object):
     """
     A class which wraps an instance of JSBSim and manages communication with it.
     """
+    encoding = 'utf-8'
+
     def __init__(self):
         root_dir = os.path.abspath("/home/gordon/Apps/jsbsim-code")
         self.sim = jsbsim.FGFDMExec(root_dir=root_dir)
+
+    def load_model(self, aircraft: str) -> None:
+        """
+        Loads the specified aircraft config into the simulation.
+
+        The root JSBSim directory aircraft folder is searched for the aircraft
+        XML config file.
+
+        :param aircraft: string, the aircraft name
+        """
+        self.sim.load_model(model=aircraft)
+
+    def get_model_name(self) -> str:
+        """ Gets the name of the aircraft model currently loaded in JSBSim.
+
+        :return: string, the name of the aircraft model if one is loaded, or
+            None if no model is loaded.
+        """
+        name: str = self.sim.get_model_name().decode(self.encoding)
+        if name:
+            return name
+        else:
+            # name is empty string, no model is loaded
+            return None
