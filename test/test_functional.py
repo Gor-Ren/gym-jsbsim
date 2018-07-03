808b9d22e4593b497da756bc8b4ceefc24715d98
Gordon <gor.rennie@gmail.com>
2018-07-03 16:50:31 +0100

    Completed FT

--- test/test_functional.py
+++ test/test_functional.py
@@ -8,7 +8,7 @@ from tasks import SteadyLevelFlightTask
 class AgentEnvInteractionTest(unittest.TestCase):
     """ Unit and functional tests for agents interacting with env. """
 
-    def test_random_agent_steady_level_task(self):
+    def test_random_agent_steady_level_task_setup(self):
         # we create an environment with the steady level flight task
         agent_interaction_hz = 8
         env = JsbSimEnv(task_type=SteadyLevelFlightTask,
@@ -23,9 +23,9 @@ class AgentEnvInteractionTest(unittest.TestCase):
         # we init a random agent with a seed
         seed = 1
         agent = RandomAgent(action_space=env.action_space, seed=seed)
+        self.assertEqual(env.action_space, agent.action_space)
 
         # this task has an action space of three controls: aileron, elevator, rudder
-        self.assertEqual(env.action_space, agent.action_space)
         self.assertEqual(3, len(agent.action_space.low))
         # we see that the action space has the correct low and high range of +-1.0
         expect_low = np.array([-1.0, -1.0, -1.0])
@@ -33,4 +33,10 @@ class AgentEnvInteractionTest(unittest.TestCase):
         np.testing.assert_array_almost_equal(expect_high, env.action_space.high)
         np.testing.assert_array_almost_equal(expect_low, env.action_space.low)
 
+        # we reset the env and receive the first state; the env is now ready
+        state = env.reset()
+        self.assertEqual(len(env.observation_space.low), len(state))
 
+        # we close the env and JSBSim closes with it
+        env.close()
+        self.assertIsNone(env.sim.sim)
