aad8a4785fe9ca0a5a8a26eb3453adabf9bc1a0b
Gordon <gor.rennie@gmail.com>
2018-07-03 17:36:16 +0100

    Add FT for random agent taking step

--- test/test_functional.py
+++ test/test_functional.py
@@ -40,3 +40,30 @@ class AgentEnvInteractionTest(unittest.TestCase):
         # we close the env and JSBSim closes with it
         env.close()
         self.assertIsNone(env.sim.sim)
+
+    def test_random_agent_steady_level_task_run(self):
+        # we create an environment and agent for the steady level flight task
+        agent_interaction_hz = 8
+        env = JsbSimEnv(task_type=SteadyLevelFlightTask,
+                        agent_interaction_freq=agent_interaction_hz)
+        seed = 1
+        agent = RandomAgent(action_space=env.action_space, seed=seed)
+
+        # we set up for a loop through one episode
+        first_state = env.reset()
+        total_reward = 0
+
+        # we take a single step
+        action = agent.act(first_state)
+        state, reward, done, info = env.step(action)
+
+        # we see the state has changed
+        self.assertEqual(first_state.shape, state.shape)
+        self.assertTrue(np.any(np.not_equal(first_state, state)),
+                        msg='state should have changed after simulation step')
+        time_step = 1.0 / agent_interaction_hz
+        self.assertAlmostEqual(time_step, env.sim.get_sim_time())
+        self.assertFalse(done, msg='episode is terminal after only a single step')
+
+        # reward should not be positive
+        self.assertGreaterEqual(0, reward)
