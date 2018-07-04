e25834228550bdf948009ef321a6a2cecc739401
Gordon <gor.rennie@gmail.com>
2018-07-04 16:51:42 +0100

    Create setup.py and MANIFEST

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='gym_jsbsim',
+      version='0.1',
+      description='A package of reinforcement learning environments for flight '
+                  'control using the JSBSim flight dynamics model.',
+      url='https://github.com/Gor-Ren/gym-jsbsim',
+      author='Gor-Ren',
+      license='MIT',
+      install_requires=[
+            'numpy',
+            'gym',
+            'matplotlib',
+      ],
+      classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)
