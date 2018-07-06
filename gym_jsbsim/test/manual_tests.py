c2d4a55c0b2314f714a2008aa965c0c01312b8a7
Gordon <gor.rennie@gmail.com>
2018-07-06 17:52:21 +0100

    Add manual test with no rendering for profiling

--- gym_jsbsim/test/manual_tests.py
+++ gym_jsbsim/test/manual_tests.py
@@ -4,7 +4,7 @@ from gym_jsbsim.environment import JsbSimEnv
 from typing import Type
 from gym_jsbsim import tasks
 from gym_jsbsim.test.stubs import TaskStub
-from gym_jsbsim.agents import RandomAgent
+from gym_jsbsim.agents import RandomAgent, ConstantAgent
 
 
 class TestJsbSimInstance(unittest.TestCase):
@@ -37,7 +37,6 @@ class TestJsbSimInstance(unittest.TestCase):
 
     def test_render_steady_level_flight_random(self):
         """ Runs steady level flight task with a random agent. """
-        seed = 1
         self.setUp(task_type=tasks.SteadyLevelFlightTask)
         agent = RandomAgent(self.env.action_space)
         render_every = 5
@@ -53,6 +52,27 @@ class TestJsbSimInstance(unittest.TestCase):
                 self.env.render(mode='human', action_names=self.env.task.action_names, action_values=action)
             step_number += 1
 
+    def test_run_episode_steady_level_flight_no_render(self):
+        self.setUp(task_type=tasks.SteadyLevelFlightTask)
+        agent = RandomAgent(self.env.action_space)
+        report_every = 20
+        EPISODES = 100
+
+        for _ in range(EPISODES):
+            ep_reward = 0
+            done = False
+            state = self.env.reset()
+            step_number = 0
+            while not done:
+                action = agent.act(state)
+                state, reward, done, info = self.env.step(action)
+                ep_reward += reward
+                if step_number % report_every == 0:
+                    print(f'time:\t{self.env.sim.get_sim_time()} s')
+                    print(f'last reward:\t{reward}')
+                    print(f'episode reward:\t{ep_reward}')
+                step_number += 1
+
 
 class FlightGearRenderTest(unittest.TestCase):
     def setUp(self, aircraft_name: str='c172p', task_type: Type[tasks.TaskModule]=TaskStub):
@@ -63,13 +83,12 @@ class FlightGearRenderTest(unittest.TestCase):
     def tearDown(self):
         self.env.close()
 
-    def test_render_steady_level_flight_random(self):
-        seed = 1
+    def test_render_steady_level_flight(self):
         self.setUp(aircraft_name='c172x', task_type=tasks.SteadyLevelFlightTask)
-        agent = RandomAgent(self.env.action_space)
+        agent = ConstantAgent(self.env.action_space)
         render_every = 5
         report_every = 20
-        EPISODES = 10
+        EPISODES = 5
 
         for _ in range(EPISODES):
             ep_reward = 0
