Fix more tests, refactor reward calculation

gym_jsbsim/environment.py
@@ -3,7 +3,7 @@ import numpy as np
 from gym_jsbsim.tasks import Task
 from gym_jsbsim.simulation import Simulation
 from gym_jsbsim.visualiser import FigureVisualiser, FlightGearVisualiser
-from typing import Type
+from typing import Type, Tuple, Dict
 
 
 class JsbSimEnv(gym.Env):
@@ -48,7 +48,7 @@ class JsbSimEnv(gym.Env):
         self.flightgear_visualiser: FlightGearVisualiser = None
         self.step_delay = None
 
-    def step(self, action: np.ndarray):
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, Dict]:
         """
         Run one timestep of the environment's dynamics. When end of
         episode is reached, you are responsible for calling `reset()`
@@ -67,7 +67,8 @@ class JsbSimEnv(gym.Env):
         if not (action.shape == self.action_space.shape):
             raise ValueError('mismatch between action and action space size')
 
-        return self.task.task_step(self.sim, action, self.sim_steps)
+        state, reward, done, info = self.task.task_step(self.sim, action, self.sim_steps)
+        return np.ndarray(state), reward, done, info
 
     def reset(self):
         """

gym_jsbsim/reward_functions.py
@@ -1,22 +1,22 @@
 import collections
 import math
 import numpy as np
+import gym_jsbsim.properties as prp
 from gym_jsbsim import utils
 from gym_jsbsim.simulation import Simulation
-from typing import Tuple
-from abc import ABC, abstractmethod
+from typing import Tuple, Callable, Union
 
 
-class ShapingReward(object):
+class Reward(object):
     """
     Immutable class storing an RL reward.
 
     We decompose rewards into tuples of component values, reflecting contributions
     from different goals. Separate tuples are maintained for the base (non-shaping)
-    components and the shaping components to allow better analysis.
+    components and the shaping components to allow analysis.
 
-    Scalar reward values can be retrieved from a ShapingReward by calling .reward()
-    or non_shaping_reward() as required.
+    Scalar reward values are retrieved by calling .reward() or non_shaping_reward().
+    The scalar value is the mean of the components.
     """
     def __init__(self, base_reward_elements: Tuple, shaping_reward_elements: Tuple):
         self.base_reward_elements = base_reward_elements
@@ -37,43 +37,101 @@ class ShapingReward(object):
         return bool(self.shaping_reward_elements)
 
 
-class Rewarder(ABC):
+class Assessor(object):
     """
-    Interface for a Rewarder, a callable object which calculates components of a reward signal.
+    Interface for a Assessor, a callable object which calculates a Reward.
 
-    We decompose the reward into a tuple of component values. A Rewarder
-    calculates each component from a simulation state and returns the tuple.
+    A Reward is decomposed into two tuples of component values. An Assessor
+    stores two corresponding tuples of functions, which calculate these values
+    from environment state.
 
-    When called,
-        rewarder(simulation, is_terminal)
-    should return a tuple of reward components.
+    An assessor object can be called thus:
+        rewarder(state, last_state, is_terminal)
+    and will return a Reward object.
     """
-    @abstractmethod
-    def __call__(self, state: np.ndarray, is_terminal: bool) -> Tuple:
-        ...
-
-    def reset(self, *args):
-        """ Called at the end of each episode, reset() should clear any state.
-
-        Default behaviour is to do nothing.
+    def __init__(self, base_reward_functions: Tuple[Callable, ...],
+                 shaping_reward_functions: Tuple[Callable, ...]=()):
         """
-        pass
+        Constructor. Sets the functions that will be used to calculate reward components.
+
+        :param base_reward_functions: collection of functions r(s, s', is_terminal)
+        :param shaping_reward_functions: optional, collection of functions f(s, s', is_terminal)
+            defaults to empty tuple resulting in no reward shaping
+        """
+        self.base_reward_functions = base_reward_functions
+        self.shaping_reward_functions = shaping_reward_functions
+
+    def __call__(self, state: np.ndarray, last_state: np.ndarray, is_terminal: bool) -> Reward:
+        """ Calculates reward from environment's state, previous state and terminal condition """
+        return Reward(tuple(R(state, last_state, is_terminal) for R in self.base_reward_functions),
+                      tuple(F(state,last_state, is_terminal) for F in self.shaping_reward_functions))
 
 
-class EmptyRewarder(object):
+class RewardFunctionFactory(object):
+    
+
+    def get_property_closeness_to_target_function(controlled_prop: prp.Property, target_prop: prp.Property,
+                                                  scale_factor: float) -> Callable:
+        """ Returns a function on States that calculates how close a controlled property is to its target.
+
+        :param controlled_prop: the property in State that is to be examined
+        :param target_prop: the property in State which is the target value, at
+            which the returned function outputs 1.0
+        :param scale_factor: float, the returned function outputs 0.25 when there
+            is this error to target. Used to account for units of measure etc.
+        :return: a function: State -> float, calculating how close the controlled prop is to target
+        """
+        def normalised_goodness_function(state: Tuple[float, ...]) -> float:
+            """
+            Calculates how "good" a state is considering we wish one state
+            variable to be equal to a target state variable.
+
+            Returns 1.0 when controlled variable is equal to target value, and
+            asymptotically approaches 0.0 as their absolute error approaches
+            infinity.
+
+            :param state: State namedtuple, the environment state
+            :return: float, how close the controlled prop is to target, 1.0 if equal, reducing to 0.0
+            """
+            actual_value = state.__getattribute__(controlled_prop)
+            target_value = state.__getattribute__(target_prop)
+            abs_error = abs(actual_value - target_value)
+            return 1 - normalise_unbounded_error(abs_error, scale_factor)
+
+        return normalised_goodness_function
+
+
+    def get_property_closeness_to_constant_function(controlled_prop: prp.Property, target_value: float,
+                                                    scale_factor: float) -> Callable:
+        """ Returns a function on States that calculates how close a controlled property is to its target.
+
+        :param controlled_prop: the property in State that is to be examined
+        :param target_value: the target for controlled_prop, at which returned function outputs 1.0
+        :param scale_factor: float, the difference at which the returned function outputs 0.75
+        :return: a function: State -> float, calculating how close the controlled prop is to target
+        """
+        def normalised_goodness_function(state: Tuple[float, ...]) -> float:
+            """
+            Calculates how "good" a state is considering we wish one state
+            variable to be equal to a constant target value.
+
+            Returns 1.0 when controlled variable is equal to target value, and
+            asymptotically approaches 0.0 as their absolute error approaches
+            infinity.
+
+            :param state: State namedtuple, the environment state
+            :return: float, how close the controlled prop is to target, 1.0 if equal, reducing to 0.0
+            """
+            actual_value = state.__getattribute__(controlled_prop)
+            abs_error = abs(actual_value - target_value)
+            return 1 - normalise_unbounded_error(abs_error, scale_factor)
+
+        return normalised_goodness_function
+
+
+class HeadingControlRewarder(Assessor):
     """
-    A Rewarder which always returns empty tuple.
-
-    Use this as your shaping rewarder when you don't want to use shaping.
-    """
-    def __call__(self, _, __) -> Tuple:
-        """ Given any Simulation and is_terminal condition, returns empty tuple. """
-        return ()
-
-
-class HeadingControlRewarder(ABC, Rewarder):
-    """
-    Abstract Rewarder for heading control tasks.
+    Abstract Assessor for heading control tasks.
 
     Contains common methods for calculating rewards or potentials for re-use by
     inheriting classes.
@@ -105,7 +163,7 @@ class HeadingControlRewarder(ABC, Rewarder):
         target_altitude_ft and its altitude in the sim is zero. Each timestep
         it can receive 1 / max_timesteps altitude keeping reward.
         """
-        altitude_ft = sim['position/h-sl-ft']
+        altitude_ft = sim[prp.altitude_sl_ft]
         altitude_error_ft = abs(self.target_altitude_ft - altitude_ft)
         norm_error = utils.normalise_unbounded_error(altitude_error_ft, self.ALTITUDE_ERROR_SCALING_FT)
         return (1 - norm_error) / self.max_timesteps
@@ -123,7 +181,7 @@ class HeadingControlRewarder(ABC, Rewarder):
     def _parallel_distance_travelled_ft(self, sim: Simulation):
         """ Calculates distance travelled in the sim parallel to the target heading """
         final_position = utils.GeodeticPosition.from_sim(sim)
-        distance_travelled_m = sim['position/distance-from-start-mag-mt']
+        distance_travelled_m = sim[prp.dist_travel_m]
         heading_travelled_deg = self.initial_position.heading_deg_to(final_position)
 
         heading_error_rad = math.radians(heading_travelled_deg - self.target_heading_deg)
@@ -178,3 +236,19 @@ class HeadingControlSimpleShapingRewarder(HeadingControlRewarder):
         shaping_reward_component = self.last_potential - new_potential
         self.last_potential = new_potential
         return shaping_reward_component
+
+
+def normalise_unbounded_error(absolute_error: float, error_scaling: float):
+    """
+    Given an error in the interval [0, +inf], returns a normalised error in [0, 1]
+
+    The normalised error asymptotically approaches 1 as absolute_error -> +inf.
+
+    The parameter error_scaling is used to scale for magnitude.
+    When absolute_error == error_scaling, the normalised error is equal to 0.75
+    """
+    if absolute_error < 0:
+        raise ValueError(f'Error to be normalised must be non-negative '
+                         f'(use abs()): {absolute_error}')
+    scaled_error = absolute_error / error_scaling
+    return (scaled_error / (scaled_error + 1)) ** 0.5

gym_jsbsim/tasks.py
@@ -2,11 +2,15 @@ import gym
 import numpy as np
 import random
 import types
+import math
 import gym_jsbsim.properties as prp
-from gym_jsbsim.properties import BoundedProperty, Property
+from gym_jsbsim import utils
+from collections import namedtuple
 from gym_jsbsim.simulation import Simulation
+from gym_jsbsim.reward_functions import Assessor, Reward
+from gym_jsbsim.properties import BoundedProperty, Property
 from abc import ABC, abstractmethod
-from typing import Optional, Sequence, Dict, Tuple
+from typing import Optional, Sequence, Dict, Tuple, Type
 
 
 class Task(ABC):
@@ -39,12 +43,12 @@ class Task(ABC):
         Initialise any state/controls and get first state observation from reset sim.
 
         :param sim: Simulation, the environment simulation
-        :return: array, the first state observation of the episode
+        :return: np array, the first state observation of the episode
         """
         ...
 
     @abstractmethod
-    def get_initial_conditions(self) -> Optional[Dict[str, float]]:
+    def get_initial_conditions(self) -> Optional[Dict[Property, float]]:
         """
         Returns dictionary mapping initial episode conditions to values.
 
@@ -78,10 +82,11 @@ class FlightTask(Task, ABC):
     Concrete subclasses should implement the following:
         state_variables attribute: tuple of Propertys, the task's state representation
         action_variables attribute: tuple of Propertys, the task's actions
+        assessor attribute: Assessor, object which calculates reward from state
         get_initial_conditions(): returns dict mapping InitialPropertys to initial values
-        _calculate_reward(): determines the step reward
         _is_done(): determines episode termination
-        (optional) _input_initial_controls(): sets simulation values at start of episode
+        (optional) _new_episode(): performs any control input/initialisation on episode reset
+        (optional) _update_custom_properties: updates any custom properties in the sim
     """
     base_state_variables = (prp.altitude_sl_ft, prp.pitch_rad, prp.roll_rad,
                             prp.u_fps, prp.v_fps, prp.w_fps,
@@ -97,14 +102,13 @@ class FlightTask(Task, ABC):
     )
     state_variables: Tuple[BoundedProperty, ...]
     action_variables: Tuple[BoundedProperty, ...]
+    assessor: Assessor
+    State: Type[Tuple]
 
-    def __init__(self, use_shaped_reward: bool=True) -> None:
-        """ Constructor
-
-        :param use_shaped_reward: use potential based reward shaping if True
-        """
-        self.use_shaped_reward = use_shaped_reward
-        self.last_reward = None
+    def __init__(self, assessor: Assessor) -> None:
+        self.last_state = None
+        self.State = namedtuple('State', [prop.name for prop in self.state_variables])
+        self.assessor = assessor
 
     def task_step(self, sim: Simulation, action: Sequence[float], sim_steps: int)\
             -> Tuple[np.ndarray, float, bool, Dict]:
@@ -116,43 +120,37 @@ class FlightTask(Task, ABC):
         for _ in range(sim_steps):
             sim.run()
 
-        obs = [sim[prop] for prop in self.state_variables]
-        reward = self._calculate_reward(sim)
-        if self.use_shaped_reward:
-            shaped_reward = reward - self.last_reward
-            self.last_reward = reward
-            reward = shaped_reward
-        done = self._is_done(sim)
-        info = {'sim_time': sim.get_sim_time()}
+        self._update_custom_properties(sim)
+        state = self.State(sim[prop] for prop in self.state_variables)
+        done = self._is_done(state, sim[prp.sim_time_s])
+        reward = self.assessor(state, self.last_state, done)
+        self.last_state = state
+        info = {'reward': reward}
 
-        return np.array(obs), reward, done, info
+        return np.array(state), reward.reward(), done, info
 
-    @abstractmethod
-    def _calculate_reward(self, sim: Simulation) -> float:
-        """ Calculates the reward from the simulation state.
-
-        :param sim: Simulation, the environment simulation
-        :return: a number, the reward for the timestep
-        """
-        ...
+    def _update_custom_properties(self, sim: Simulation) -> None:
+        """ Calculates any custom properties which change every timestep. """
+        pass
 
     @abstractmethod
-    def _is_done(self, sim: Simulation) -> bool:
+    def _is_done(self, state: Tuple[float, ...], episode_time: float) -> bool:
         """ Determines whether the current episode should terminate.
 
-        :param sim: Simulation, the environment simulation
+        :param state: the last state observation
+        :param episode_time: the episode time in seconds
         :return: True if the episode should terminate else False
         """
         ...
 
     def observe_first_state(self, sim: Simulation) -> np.ndarray:
-        self._input_initial_controls(sim)
-        state = [sim[prop] for prop in self.state_variables]
-        if self.use_shaped_reward:
-            self.last_reward = self._calculate_reward(sim)
-        return np.array(state)
+        self._new_episode(sim)
+        self._update_custom_properties(sim)
+        state = self.State(sim[prop] for prop in self.state_variables)
+        self.last_state = state
+        return np.ndarray(state)
 
-    def _input_initial_controls(self, sim: Simulation) -> None:
+    def _new_episode(self, sim: Simulation) -> None:
         """
         This method is called at the start of every episode. It is used to set
         the value of any controls or environment properties not already defined
@@ -183,19 +181,20 @@ class SteadyLevelFlightTask(FlightTask):
     current heading.
     """
     MAX_TIME_SECS = 15
-    MIN_ALT_FT = 1000
-    TOO_LOW_REWARD = -10
     THROTTLE_CMD = 0.8
     MIXTURE_CMD = 0.8
     INITIAL_HEADING_DEG = 270
+    target_heading_deg = BoundedProperty('target/heading-deg', 'desired heading [deg]',
+                                         prp.heading_deg.min, prp.heading_deg.max)
+    distance_parallel_to_heading_m = Property('target/dist-parallel-heading-m',
+                                              'distance travelled parallel to target heading [m]')
 
-    extra_state_variables = (prp.heading_deg,)
+    extra_state_variables = (prp.heading_deg, target_heading_deg, distance_parallel_to_heading_m)
+    state_variables = FlightTask.base_state_variables + extra_state_variables
     action_variables = (prp.aileron_cmd, prp.elevator_cmd, prp.rudder_cmd)
 
-    def __init__(self, ):
-        super().__init__()
-        self.state_variables = super().base_state_variables + self.extra_state_variables
-        self.target_values = self._make_target_values()
+    def __init__(self, assessor: Assessor):
+        super().__init__(assessor)
 
     def _make_target_values(self):
         """
@@ -243,36 +242,42 @@ class SteadyLevelFlightTask(FlightTask):
                             }
         return {**self.base_initial_conditions, **extra_conditions}
 
-    def _calculate_reward(self, sim: Simulation) -> float:
-        reward = 0
-        for prop, target_value, gain in self.target_values:
-            reward -= abs(target_value - sim[prop]) * gain
-        too_low = sim[prp.altitude_sl_ft] < self.MIN_ALT_FT
-        if too_low:
-            reward += self.TOO_LOW_REWARD
-        return reward
+    def _update_custom_properties(self, sim: Simulation) -> None:
+        self._update_parallel_distance_travelled(sim, self.INITIAL_HEADING_DEG)
 
-    def _is_done(self, sim: Simulation) -> bool:
-        time_out = sim.get_sim_time() > self.MAX_TIME_SECS
-        too_low = sim[prp.altitude_sl_ft] < self.MIN_ALT_FT
-        return time_out or too_low
+    def _update_parallel_distance_travelled(self, sim: Simulation, target_heading_deg: float) -> None:
+        """ Calculates how far aircraft has travelled from initial position parallel to target heading
 
-    def _input_initial_controls(self, sim: Simulation) -> None:
-        # start engines and trims for steady, level flight
+         Stores result in Simulation as custom property.
+         """
+        current_position = utils.GeodeticPosition.from_sim(sim)
+        heading_travelled_deg = self.initial_position.heading_deg_to(current_position)
+        heading_error_rad = math.radians(heading_travelled_deg - target_heading_deg)
+
+        distance_travelled_m = sim[prp.dist_travel_m]
+        parallel_distance_travelled_m = math.cos(heading_error_rad) * distance_travelled_m
+        sim[self.distance_parallel_to_heading_m] = parallel_distance_travelled_m
+
+    def _is_done(self, state: Tuple[float, ...], episode_time: float) -> bool:
+        return episode_time > self.MAX_TIME_SECS
+
+    def _new_episode(self, sim: Simulation) -> None:
         sim.start_engines()
         sim[prp.throttle_cmd] = self.THROTTLE_CMD
         sim[prp.mixture_cmd] = self.MIXTURE_CMD
+
+        sim[self.target_heading_deg] = self._get_target_heading()
+        self.initial_position = utils.GeodeticPosition.from_sim(sim)
+
+    def _get_target_heading(self) -> float:
+        # use the same, initial heading every episode
+        return self.INITIAL_HEADING_DEG
 
 
 class HeadingControlTask(SteadyLevelFlightTask):
     """
     A task in which the agent must make a turn and fly level on a desired heading.
     """
-    HEADING_MIN = 0
-    HEADING_MAX = 360
-    target_heading_deg = BoundedProperty('target/heading-deg', 'desired heading [deg]',
-                                         prp.heading_deg.min, prp.heading_deg.max)
-    extra_state_variables = (prp.heading_deg, target_heading_deg)
 
     def _make_target_values(self):
         """ Sets an attribute specifying the desired state of the aircraft.
@@ -287,7 +292,7 @@ class HeadingControlTask(SteadyLevelFlightTask):
         super_target_values = super()._make_target_values()
         heading_target_index = 4
         old_heading_triple = super_target_values[heading_target_index]
-        random_target_heading = random.uniform(self.HEADING_MIN, self.HEADING_MAX)
+        random_target_heading = random.uniform(prp.heading_deg.min, prp.heading_deg.max)
         new_heading_triple = (old_heading_triple[0], random_target_heading, old_heading_triple[2])
 
         target_values = []
@@ -305,7 +310,10 @@ class HeadingControlTask(SteadyLevelFlightTask):
         initial_conditions[prp.initial_heading_deg] = random_heading
         return initial_conditions
 
-    def _input_initial_controls(self, sim: Simulation) -> None:
-        sim[self.target_heading_deg] = random.uniform(self.target_heading_deg.min,
-                                                           self.target_heading_deg.max)
-        return super()._input_initial_controls(sim)
+    def _update_custom_properties(self, sim: Simulation) -> None:
+        self._update_parallel_distance_travelled(sim, sim[self.target_heading_deg])
+
+    def _get_target_heading(self) -> float:
+        # select a random heading each episode
+        return random.uniform(self.target_heading_deg.min,
+                              self.target_heading_deg.max)