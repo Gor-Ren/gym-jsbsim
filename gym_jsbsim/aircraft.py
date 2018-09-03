cbbae3e41841fd1f72c214694dd1847ac44a53ee
Gordon <gor.rennie@gmail.com>
2018-09-03 16:00:57 +0100

    Increase episode time to 200 s, reduce distance traveled margin to 0.1

--- gym_jsbsim/aircraft.py
+++ gym_jsbsim/aircraft.py
@@ -7,7 +7,7 @@ class Aircraft(collections.namedtuple('Aircraft', ['id', 'name', 'cruise_speed_k
 
     def get_max_distance_m(self, episode_time_s: float) -> float:
         """ Estimates the maximum distance this aircraft can travel in an episode """
-        margin = 0.2
+        margin = 0.1
         return self.cruise_speed_kts * self.KTS_TO_M_PER_S * episode_time_s * (1 + margin)
 
     def get_cruise_speed_fps(self) -> float:
